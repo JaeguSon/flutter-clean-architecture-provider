65e279dc6000c0ee1886f737cb8fe6c1347f5677
son-home <sonjg@hianddtech.com>
2023-07-08 23:06:04 +0900

    [feat] routing methods

--- lib/config/router/route_navigator.dart
+++ lib/config/router/route_navigator.dart
@@ -6,4 +6,43 @@ class RouteNavigator {
   static Map<String, WidgetBuilder> navigationMap = {
     RouteName.splash: (context) => const SplashPage(),
   };
+
+  RouteNavigator._privateConstructor();
+  static final RouteNavigator _instance = RouteNavigator._privateConstructor();
+
+  factory RouteNavigator() {
+    return _instance;
+  }
+
+  _getPageFrom({required String name}) {
+    final route = navigationMap[name];
+
+    if (route == null) {
+      return const SplashPage();
+    }
+
+    return route;
+  }
+
+  toPageAndRoot(BuildContext context, {required String to}) {
+    Navigator.pushAndRemoveUntil(
+      context,
+      PageRouteBuilder(pageBuilder: (_, __, ___) => _getPageFrom(name: to)),
+      (route) => false,
+    );
+  }
+
+  toPage(BuildContext context, {required String to}) {
+    Navigator.push(
+      context,
+      PageRouteBuilder(pageBuilder: (_, __, ___) => _getPageFrom(name: to)),
+    );
+  }
+
+  toPrevious(BuildContext context) {
+    if (Navigator.canPop(context)) {
+      Navigator.pop(context);
+      return;
+    }
+  }
 }
